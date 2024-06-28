9e4dc344c17cc10a001bdd408a7e453588646123
ysuolmai <fan0417@gmail.com>
2024-06-29 07:52:24 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -56,9 +56,9 @@ fi
 #process .config file
 sed -i '/usb/d' ./.config
 #sed -i '/passwall/d' ./.config
-sed -i '/v2ray/d' ./.config
-sed -i '/sing-box/d' ./.config
-sed -i '/SINGBOX/d' ./.config
+#sed -i '/v2ray/d' ./.config
+#sed -i '/sing-box/d' ./.config
+#sed -i '/SINGBOX/d' ./.config
 sed -i '/openclash/d' ./.config
 #sed -i '/qihoo_v6/d' ./.config
 #sed -i '/redmi_ax5=y/d' ./.config
@@ -71,7 +71,7 @@ sed -i '/uugamebooster/d' ./.config
 sed -i '/autosamba/d' ./.config
 sed -i '/samba/d' ./.config
 sed -i '/zerotier/d' ./.config
-sed -i '/homeproxy/d' ./.config
+#sed -i '/homeproxy/d' ./.config
 
 
 provided_config_lines=(
@@ -82,8 +82,8 @@ provided_config_lines=(
 "CONFIG_PACKAGE_luci-app-zerotier=y"
 "CONFIG_PACKAGE_luci-i18n-zerotier-zh-cn=y"
 "CONFIG_PACKAGE_luci-app-adguardhome=y"
-"CONFIG_PACKAGE_luci-app-passwall=y"
-"CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y"
+#"CONFIG_PACKAGE_luci-app-passwall=y"
+#"CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y"
 #"CONFIG_PACKAGE_luci-app-ssr-plus=y"
 #"CONFIG_PACKAGE_luci-i18n-ssr-plus-zh-cn=y"
 "CONFIG_PACKAGE_luci-app-poweroff=y"
