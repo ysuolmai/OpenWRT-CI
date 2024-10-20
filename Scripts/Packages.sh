7607b2c24f96341352b94e9a2bb0b3c1c219ac25
ysuolmai <fan0417@gmail.com>
2024-10-20 22:40:41 +0800

    Update Packages.sh
    Update homeproxy source

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -24,7 +24,7 @@ UPDATE_PACKAGE() {
 UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
 
-UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
+#UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
 UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main"
 UPDATE_PACKAGE "nekoclash" "Thaolga/luci-app-nekoclash" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
@@ -47,7 +47,7 @@ UPDATE_PACKAGE "luci-app-poweroff" "esirplayground/luci-app-poweroff" "master"
 UPDATE_PACKAGE "luci-app-tailsacle" "asvow/luci-app-tailscale" "main"
 #UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
 UPDATE_PACKAGE "luci-app-lucky" "gdy666/luci-app-lucky" "main"
-
+UPDATE_PACKAGE "homeproxy" "bulianglin/homeproxy" "master"
 
 #更新软件包版本
 UPDATE_VERSION() {
